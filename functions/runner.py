913fe7771ea41f58e3813df0bea927545263e5ec
Shawn Dsouza <shawn27.dsouza@pm.me>
2024-09-14 12:44:28 +0530

    add text to model output plots

--- functions/runner.py
+++ functions/runner.py
@@ -46,11 +46,17 @@ def plot_pop(m = None, file = 'model_pop.png'):
 
     data = m.count.get_model_vars_dataframe()
     
+    params = m.kwargs
+    
+    ## remove columns whose names contain _
+    
+    data = data[[col for col in data.columns if '_' not in col]]
+    
     ## plot the number of prey, predator and resource agents over time
 
     print('Plotting number of agents over time...')
 
-    fig = plt.figure(figsize=(10, 5))
+    fig = plt.figure(figsize=(10,6))
     
     colors = ['blue', 'red','green']
     
@@ -67,7 +73,19 @@ def plot_pop(m = None, file = 'model_pop.png'):
     plt.xlabel('Time')
     plt.ylabel('Number of agents')  
     plt.title('Number of agents over time')
+    
+    # print praameters below the plot
+    text = [f'{key} = {params[key]}' for key in params if key != 'model' and key != 'progress' and key != 'info']
+    
+    # make the text a single string
+    
+    text = ', '.join(text)
+    
+    plt.figtext(0.5, 0.1, text, ha="center", fontsize=10, wrap=True,
+                bbox={"facecolor":"white", "alpha":0.5, "pad":10,})
 
+    fig.subplots_adjust(bottom=0.25)
+    
     plt.legend()
 
     # save the plot
@@ -126,7 +144,7 @@ def plot_space(m = None, steps = 100, duration = 60, file = 'space.gif'):
     ani.save(file, writer='imagemagick', fps=steps/duration)
     
     return ani    
-
+    
 ## create a function to animate the density of agents over time
 
 def plot_density(m = None, duration = 60, steps = 100, file = 'density.gif'):
