b38e3d2bb9c3b709dbae0d840108d0786c228a54
Shawn Dsouza <shawn27.dsouza@pm.me>
2024-09-15 18:37:09 +0530

    added plots to analysis

--- analysis.R
+++ analysis.R
@@ -2,11 +2,32 @@
 # load libraries
 
 library(pacman)
-p_load(dplyr, tidyr, ggplot2)
+p_load(dplyr, tidyr, ggplot2, viridis, cowplot, gridExtra)
 
-# import data
 
-data <- read.csv("lv_results.csv")
+# loop through models, skip if file does not exist
+
+super = read.csv("output/super_results.csv")
+apex = read.csv("output/apex_results.csv")
+lv = read.csv("output/lv_results.csv")
+
+super$model = "super"
+apex$model = "apex"
+lv$model = "lv"
+
+max_steps = 999
+
+# add missing columns
+
+apex$super_target = NA
+apex$super_lethality = NA
+
+lv$super_target = NA
+lv$super_lethality = NA
+
+# combine data
+
+data = rbind(super, apex, lv)
 
 # summarise data
 
@@ -17,50 +38,114 @@ summary(data)
 
 data_scored = data%>%
 mutate(outcome = ifelse(Predator == 0, "Prey Wins", 
-                            ifelse(Prey == 0, "Exctinction", 
+                            ifelse(Prey == 0, "Extinction", 
                                     "Coexistance")),
+        outcome = ifelse(step == max_steps, outcome, ifelse(outcome == "Coexistance" & Predator > Prey, "Extinction", outcome)),
         score = ifelse(outcome == "Prey Wins", 0, 
-                        ifelse(outcome == "Extinction", -1, 1)),
-        strat = ifelse(prey_info == 0 & predator_info == 0, "Naive",
-                       ifelse(prey_info == 1 & predator_info == 0, "Prey Informed",
-                              ifelse(prey_info == 0 & predator_info == 1, "Predator Informed",
-                                     "Both Informed"))))
+                        ifelse(outcome == "Extinction", -1, 1)))
 
-# mean scores
 
-data_summary = data_scored%>%
-    group_by(strat, s_energy, s_breed, f_breed, f_die)%>%
-    summarise(mean_score = mean(score),
-              sd_score = sd(score),
-              n = n())
+# relationship between model parameters and outcome
 
-# plot grid of s_breed vs f_breed
+## prameaters: 
+## lv: s_breed, f_breed, f_die, s_energy
+## apex: s_breed, f_breed, f_die, s_energy
+## super: s_breed, f_breed, f_die, s_energy, super_target, super_lethality
 
-ggplot(data_summary, aes(x = s_breed, y = f_breed, fill = mean_score))+
+# plot 1: mean score by s_breed vs f_breed for each f_die, grid for each s_energy
+
+# lv model
+
+data_scored%>%
+    filter(model == "lv")%>%
+    group_by(s_breed, f_breed, f_die, s_energy)%>%
+    summarise(mean_score = mean(score), sd_score = sd(score), n = n())%>%
+    ggplot(aes(x = s_breed, y = f_breed, fill = mean_score))+
     geom_tile()+
-    scale_fill_viridis_c()+
-    facet_grid(strat~f_die, scales = "free")+
+    facet_grid(f_die~s_energy)+
     theme_bw()+
-    theme(text = element_text(size = 30),
-    legend.position = "bottom",
-    # increase size of legend key
-    legend.key.size = unit(20, "mm"),
-    # increase spaing for legend title
-    legend.title = element_text(size = 20))
+    scale_fill_viridis(name = "Outcome", 
+    # limits for color scale
+    limits = c(-1, 1)
+    )+
+    theme(legend.position = "top",
+    text = element_text(size =  20),
+    # legend width
+    legend.key.width = unit(2, "cm"),
+    # legend height
+    legend.key.height = unit(0.5, "cm"))+
+    labs(x = "Predator Breeding Rate", y = "Prey Breeding Rate")
+  
+ggsave("output/figures/lv_params.png", width = 10, height = 10, dpi = 300)
 
-ggsave("score_grid_f-die.png", height = 24, width = 30, dpi = 300)
+# apex model
 
-# plot grid of s_breed vs f_breed
-
-ggplot(data_summary, aes(x = s_breed, y = f_breed, fill = mean_score))+
+data_scored%>%
+    filter(model == "apex")%>%
+    group_by(s_breed, f_breed, f_die, s_energy)%>%
+    summarise(mean_score = mean(score), sd_score = sd(score), n = n())%>%
+    ggplot(aes(x = s_breed, y = f_breed, fill = mean_score))+
     geom_tile()+
-    scale_fill_viridis_c()+
-    facet_grid(strat~s_energy, scales = "free")+
+    facet_grid(f_die~s_energy)+
     theme_bw()+
-    theme(text = element_text(size = 30),
-    legend.position = "bottom",
-    # increase size of legend key
-    legend.key.size = unit(20, "mm"))
+    scale_fill_viridis(name = "Outcome", limits = c(-1, 1))+
+    theme(legend.position = "top",
+    text = element_text(size =  20),
+    # legend width
+    legend.key.width = unit(2, "cm"),
+    # legend height
+    legend.key.height = unit(0.5, "cm"))+
+    labs(x = "Predator Breeding Rate", y = "Prey Breeding Rate")
+
+ggsave("output/figures/apex_params.png", width = 10, height = 10, dpi = 300)
+
+# super model
+
+# create grid for super_target and super_lethality
+
+super_target = c("Prey", "Predator", "Both")
+super_lethality = c("Lethal", "Non-lethal")
+
+super_data_scored = data_scored%>%
+    filter(model == "super")%>%
+    mutate(super_target = ifelse(super_target == 1, "Prey", 
+                                 ifelse(super_target == 2, "Predator", "Both")),
+        super_lethality = ifelse(super_lethality == 1, "Lethal", "Non-lethal"))%>%
+    group_by(s_breed, f_breed, f_die, s_energy, super_target, super_lethality)%>%
+    summarise(mean_score = mean(score), sd_score = sd(score), n = n())
+
+# create ggplot grid for each super_target and super_lethality
+
+plots = list()
+
+for (i in super_target){
     
+    for (j in super_lethality){
+        
+        plot = super_data_scored%>%
+            filter(super_target == i, super_lethality == j)%>%
+            ggplot(aes(x = s_breed, y = f_breed, fill = mean_score))+
+            geom_tile()+
+            facet_grid(f_die~s_energy)+
+            theme_bw()+
+            scale_fill_viridis(name = "Outcome", limits = c(-1, 1))+
+            theme(legend.position = "top",
+            text = element_text(size =  20),
+            # legend width
+            legend.key.width = unit(2, "cm"),
+            # legend height
+            legend.key.height = unit(0.5, "cm"))+
+            labs(x = "Predator Breeding Rate", y = "Prey Breeding Rate",
+                title = paste("Super Model: Target = ", i, ", Lethality = ", j))+
+            # center title
+            theme(plot.title = element_text(hjust = 0.5))
+        
+        plots[[paste(i, j)]] = plot
+    
+    }
 
-ggsave("score_grid_s-energy.png", height = 24, width = 30, dpi = 300)
+}
+
+super_plot = grid.arrange(grobs = plots, ncol = 2, nrow = 3)
+
+ggsave(super_plot, file = "output/figures/super_params.png", width = 20, height = 30, dpi = 300)
