93a6f1078d642a981e4874d76bfb9e286e2ebd8e
Shawn Dsouza <shawn27.dsouza@pm.me>
2024-09-18 20:18:24 +0530

    plot strategy examples

--- plot_strat.py
+++ plot_strat.py
@@ -1,11 +1,6 @@
 from functions.runner import plot_pop, plot_density, plot_space, plot_space_pop, plot_age, plot_energy, plot_nnd
 import pandas as pd
 
-# load data
-
-model_data = pd.read_csv('data_model_lv.csv')
-agent_data = pd.read_csv('data_agents_lv.csv')
-
 params = {
     
     # model to run
@@ -45,23 +40,23 @@ for strategy in strategies:
     # set info parameters
     
     if strategy == 'Both Naive':
-        file = 'data_agents_lv_naive.csv'
-        file2 = 'data_model_lv_naive.csv'
+        file = 'data_agents_lv_Both Naive.csv'
+        file2 = 'data_model_lv_Both Naive.csv'
         params['prey_info'] = False
         params['predator_info'] = False
     elif strategy == 'Predator Informed':
-        file = 'data_agents_lv_pred.csv'
-        file2 = 'data_model_lv_pred.csv'
+        file = 'data_agents_lv_Predator Informed.csv'
+        file2 = 'data_model_lv_Predator Informed.csv'
         params['prey_info'] = False
         params['predator_info'] = True
     elif strategy == 'Prey Informed':
-        file = 'data_agents_lv_prey.csv'
-        file2 = 'data_model_lv_prey.csv'
+        file = 'data_agents_lv_Prey Informed.csv'
+        file2 = 'data_model_lv_Prey Informed.csv'
         params['prey_info'] = True
         params['predator_info'] = False
     elif strategy == 'Both Informed':
-        file = 'data_agents_lv_both.csv'
-        file2 = 'data_model_lv_both.csv'
+        file = 'data_agents_lv_Both Informed.csv'
+        file2 = 'data_model_lv_Both Informed.csv'
         params['prey_info'] = True
         params['predator_info'] = True
 
@@ -74,15 +69,15 @@ for strategy in strategies:
     
     # plot the number of agents over time
 
-    plot_pop(model_data=model_data, params = params, file = 'pop_lv.png', steps=steps)
+    plot_pop(model_data=model_data, params = params, file = f'pop_{strategy.replace(" ", "_")}.png', steps=steps)
 
     # plot the age distribution of agents
 
-    plot_age(agent_data=agent_data, file = 'age_lv.png', steps=steps)
+    plot_age(agent_data=agent_data, file = f'age_{strategy.replace(" ", "_")}.png', steps=steps)
 
     # plot the energy distribution of agents
 
-    plot_energy(agent_data=agent_data, file = 'energy_lv.png', steps=steps)
+    plot_energy(agent_data=agent_data, file = f'energy_{strategy.replace(" ", "_")}.png', steps=steps)
 
     # plot the distance distribution of agents
 
@@ -90,17 +85,17 @@ for strategy in strategies:
 
     # plot the nearest neighbour distance distribution of agents
 
-    plot_nnd(agent_data=agent_data, file = 'nnd_lv.png', steps=steps)
+    plot_nnd(agent_data=agent_data, file = f'nnd_{strategy.replace(" ", "_")}.png', steps=steps)
 
     # plot density of agents
 
-    plot_density(spatial_data=agent_data, file = 'density_lv.gif', steps=steps)
+    plot_density(spatial_data=agent_data, file = f'density_{strategy.replace(" ", "_")}.gif', steps=steps)
 
     # plot spatial distribution of agents
 
-    plot_space(agent_data=agent_data,file = 'space_lv.gif', steps=steps)
+    plot_space(agent_data=agent_data,file = f'space_{strategy.replace(" ", "_")}.gif', steps=steps)
 
     # plot spatial distribution of agents with population size
 
     plot_space_pop(agent_data=agent_data, model_data=model_data, params=params,
-                file = 'space_pop_lv.gif', steps=steps)
+                file = f'space_pop_{strategy.replace(" ", "_")}.gif', steps=steps)
