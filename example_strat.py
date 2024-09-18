dab298dbfd1e2e286486849518faa9203341ef77
Shawn Dsouza <shawn27.dsouza@pm.me>
2024-09-18 11:06:34 +0530

    added examples for predator and prey strategies

---
+++ example_strat.py
@@ -0,0 +1,76 @@
+from functions.runner import model_run
+
+# set model parameters
+
+kwargs = {
+    
+    # model to run
+    'model': 'lv',
+    'progress': True,
+    'info' : False,    
+    'limit' : 50*50*4,
+    
+    # model parameters
+    'width': 50,
+    'height': 50,
+    
+    # number of agents to start with
+    'predator': 500,
+    'prey': 500,
+    
+    ## prey traits
+    'prey_info': True,
+    'f_breed': 0.5, # max birth rate
+    'f_die': 0.1,
+    'f_max': 2500,
+    'risk_cost': 0,
+    
+    ## predator traits
+    'predator_info': True,
+    's_energy': 10,
+    's_breed': 0.1,
+    's_lethality': 0.5,
+}
+
+steps = 1000
+
+strategies = ['Both Naive', 'Predator Informed', 'Prey Informed', 'Both Informed']
+
+for strategy in strategies:
+    
+    # set info parameters
+    
+    if strategy == 'Both Naive':
+        
+        kwargs['prey_info'] = False
+        kwargs['predator_info'] = False
+        
+    elif strategy == 'Predator Informed':
+        
+        kwargs['prey_info'] = False
+        kwargs['predator_info'] = True
+    elif strategy == 'Prey Informed':
+        
+        kwargs['prey_info'] = True
+        kwargs['predator_info'] = False
+        
+    elif strategy == 'Both Informed':
+            
+            kwargs['prey_info'] = True
+            kwargs['predator_info'] = True
+            
+    else:
+        
+        print('Invalid Strategy')
+        
+        break
+
+    # run the model
+    m = model_run(**kwargs, steps=steps)
+
+    # save data
+
+    model_data = m.count.get_model_vars_dataframe()
+    model_data.to_csv(f'data_model_{kwargs['model']}_{strategy}.csv')
+    agent_data = m.spatial.get_agent_vars_dataframe()
+    agent_data.to_csv(f'data_agents_{kwargs['model']}_{strategy}.csv')
---
+++ plot_strat.py
@@ -0,0 +1,106 @@
+from functions.runner import plot_pop, plot_density, plot_space, plot_space_pop, plot_age, plot_energy, plot_nnd
+import pandas as pd
+
+# load data
+
+model_data = pd.read_csv('data_model_lv.csv')
+agent_data = pd.read_csv('data_agents_lv.csv')
+
+params = {
+    
+    # model to run
+    'model': 'lv',
+    'progress': True,
+    'info' : False,    
+    'limit' : 50*50*4,
+    
+    # model parameters
+    'width': 50,
+    'height': 50,
+    
+    # number of agents to start with
+    'predator': 500,
+    'prey': 500,
+    
+    ## prey traits
+    'prey_info': True,
+    'f_breed': 0.5, # max birth rate
+    'f_die': 0.1,
+    'f_max': 2500,
+    'risk_cost': 0,
+    
+    ## predator traits
+    'predator_info': True,
+    's_energy': 10,
+    's_breed': 0.1,
+    's_lethality': 0.5,
+}
+
+steps = 1000
+
+strategies = ['Both Naive', 'Predator Informed', 'Prey Informed', 'Both Informed']
+
+for strategy in strategies:
+    
+    # set info parameters
+    
+    if strategy == 'Both Naive':
+        file = 'data_agents_lv_naive.csv'
+        file2 = 'data_model_lv_naive.csv'
+        params['prey_info'] = False
+        params['predator_info'] = False
+    elif strategy == 'Predator Informed':
+        file = 'data_agents_lv_pred.csv'
+        file2 = 'data_model_lv_pred.csv'
+        params['prey_info'] = False
+        params['predator_info'] = True
+    elif strategy == 'Prey Informed':
+        file = 'data_agents_lv_prey.csv'
+        file2 = 'data_model_lv_prey.csv'
+        params['prey_info'] = True
+        params['predator_info'] = False
+    elif strategy == 'Both Informed':
+        file = 'data_agents_lv_both.csv'
+        file2 = 'data_model_lv_both.csv'
+        params['prey_info'] = True
+        params['predator_info'] = True
+
+    # load data
+
+    model_data = pd.read_csv(file2)
+    agent_data = pd.read_csv(file)
+
+    print(f'Plotting {strategy} Strategy....')
+    
+    # plot the number of agents over time
+
+    plot_pop(model_data=model_data, params = params, file = 'pop_lv.png', steps=steps)
+
+    # plot the age distribution of agents
+
+    plot_age(agent_data=agent_data, file = 'age_lv.png', steps=steps)
+
+    # plot the energy distribution of agents
+
+    plot_energy(agent_data=agent_data, file = 'energy_lv.png', steps=steps)
+
+    # plot the distance distribution of agents
+
+    #plot_dist(agent_data=agent_data, file = 'dist_lv.png')
+
+    # plot the nearest neighbour distance distribution of agents
+
+    plot_nnd(agent_data=agent_data, file = 'nnd_lv.png', steps=steps)
+
+    # plot density of agents
+
+    plot_density(spatial_data=agent_data, file = 'density_lv.gif', steps=steps)
+
+    # plot spatial distribution of agents
+
+    plot_space(agent_data=agent_data,file = 'space_lv.gif', steps=steps)
+
+    # plot spatial distribution of agents with population size
+
+    plot_space_pop(agent_data=agent_data, model_data=model_data, params=params,
+                file = 'space_pop_lv.gif', steps=steps)
